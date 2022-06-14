bae0ac59aeedd108974cf75b721ac9d21284744b
QuocTuanIT <kutuanonline199@gmail.com>
2022-06-14 14:38:47 +0700

    Part8 - Refactor Folder Structure

--- manage.py
+++ manage.py
@@ -6,11 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.base")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
     try:
         from django.core.management import execute_from_command_line
-    except ImportError as exc:  # pragma: no cover
-        raise ImportError(  # pragma: no cover
+    except ImportError as exc:
+        raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
@@ -18,5 +18,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
